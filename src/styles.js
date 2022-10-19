09bf075a38efb4ed53cb1199541b18c45146aa40
Roberto Menegassi <robertomenegassi@hotmail.com>
2022-10-19 17:00:26 -0300

    feat: Styles

---
+++ src/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+  background-color: #2c3e50;
+  flex: 1;
+`;
+
+export const BoxItems = styled.View`
+  flex: 1;
+`;
+
+export const BoxMatch = styled.Text`
+  text-align: center;
+  font-size: 20px;
+  color: #ecf0f1;
+`;
